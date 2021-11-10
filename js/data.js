2480254b1e56fe07d13d958b0174f0cd3bffcf6c
Kouvuli <Kouvuli@users.noreply.github.com>
2021-11-10 12:05:00 +0700

    button nav first commit

---
+++ js/data.js
@@ -0,0 +1,134 @@
+let products = [
+  {
+    id: 1,
+    name: "JBL E55BT KEY BLACK",
+    image1: "./images/JBL_E55BT_KEY_BLACK_6175_FS_x1-1605x1605px.png",
+    image2: "./images/JBL_LIVE650BTNC_Product Image_Folded_Black.webp",
+    old_price: "400",
+    curr_price: "300",
+  },
+  {
+    id: 2,
+    name: "JBL JR 310BT",
+    image1: "./images/JBL_JR 310BT_Product Image_Hero_Skyblue.png",
+    image2: "./images/JBL_JR 310BT_Product Image_Detail_Skyblue.png",
+    old_price: "400",
+    curr_price: "300",
+  },
+  {
+    id: 3,
+    name: "JBL TUNE 750BTNC",
+    image1:
+      "./images/kisspng-beats-electronics-headphones-apple-beats-studio-red-headphones.png",
+    image2: "./images/JBL_E55BT_KEY_RED_6063_FS_x1-1605x1605px.webp",
+    old_price: "400",
+    curr_price: "300",
+  },
+  {
+    id: 4,
+    name: "JBL Horizon",
+    image1: "./images/JBLHorizon_001_dvHAMaster.png",
+    image2: "./images/JBLHorizon_BLK_002_dvHAMaster.webp",
+    old_price: "400",
+    curr_price: "300",
+  },
+  {
+    id: 5,
+    name: "JBL Tune 220TWS",
+    image1: "./images/JBL_TUNE220TWS_ProductImage_Pink_ChargingCaseOpen.png",
+    image2: "./images/JBL_TUNE220TWS_ProductImage_Pink_Back.png",
+    old_price: "400",
+    curr_price: "300",
+  },
+  {
+    id: 6,
+    name: "UA Project Rock",
+    image1:
+      "./images/190402_E1_FW19_EarbudsWCase_S13_0033-1_1605x1605_HERO.png",
+    image2:
+      "./images/190402_E1_FW19_EarbudsWCase_S13_0033-1_1605x1605_BACK.png",
+    old_price: "400",
+    curr_price: "300",
+  },
+  {
+    id: 7,
+    name: "JBL Endurance SPRINT",
+    image1:
+      "./images/JBL_Endurance-SPRINT_Product-Image_Red_front-1605x1605px.webp",
+    image2: "./images/JBL-Endurance-Sprint_Alt_Red-1605x1605px.webp",
+    old_price: "400",
+    curr_price: "300",
+  },
+  {
+    id: 8,
+    name: "",
+    image1: "",
+    image2: "",
+    image3: "",
+    price: "",
+    discount: "",
+  },
+  {
+    id: 9,
+    name: "",
+  },
+  {
+    id: 10,
+    name: "",
+  },
+  {
+    id: 11,
+    name: "",
+  },
+  {
+    id: 12,
+    name: "",
+  },
+  {
+    id: 13,
+    name: "",
+  },
+  {
+    id: 14,
+    name: "",
+  },
+  {
+    id: 15,
+    name: "",
+  },
+  {
+    id: 16,
+    name: "",
+  },
+  {
+    id: 17,
+    name: "",
+  },
+  {
+    id: 18,
+    name: "",
+  },
+  {
+    id: 19,
+    name: "",
+  },
+  {
+    id: 20,
+    name: "",
+  },
+  {
+    id: 21,
+    name: "",
+    image1:
+      "https://taingheviet.com/uploads/product/jbl-endurance-jump-74gf6-products-image-890-1539663124-pulyt.jpg",
+    image2: "",
+  },
+  {
+    id: 22,
+    name: "",
+  },
+  {
+    id: 23,
+    name: "",
+  },
+];
