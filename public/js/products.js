ec19c23381074f43ed57e8fa7a026700009510da
Kouvuli <Kouvuli@users.noreply.github.com>
2021-11-11 22:41:32 +0700

    convert to handlebar views

--- public/js/products.js
+++ public/js/products.js
@@ -1,53 +1,56 @@
 let products = [
-    {
-        name: 'JBL E55BT KEY BLACK',
-        image1: './images/JBL_E55BT_KEY_BLACK_6175_FS_x1-1605x1605px.png',
-        image2: './images/JBL_LIVE650BTNC_Product Image_Folded_Black.webp',
-        old_price: '400',
-        curr_price: '300'
-    },
-    {
-        name: 'JBL JR 310BT',
-        image1: './images/JBL_JR 310BT_Product Image_Hero_Skyblue.png',
-        image2: './images/JBL_JR 310BT_Product Image_Detail_Skyblue.png',
-        old_price: '400',
-        curr_price: '300'
-    },
-    {
-        name: 'JBL TUNE 750BTNC',
-        image1: './images/kisspng-beats-electronics-headphones-apple-beats-studio-red-headphones.png',
-        image2: './images/JBL_E55BT_KEY_RED_6063_FS_x1-1605x1605px.webp',
-        old_price: '400',
-        curr_price: '300'
-    },
-    {
-        name: 'JBL Horizon',
-        image1: './images/JBLHorizon_001_dvHAMaster.png',
-        image2: './images/JBLHorizon_BLK_002_dvHAMaster.webp',
-        old_price: '400',
-        curr_price: '300'
-    },
-    {
-        name: 'JBL Tune 220TWS',
-        image1: './images/JBL_TUNE220TWS_ProductImage_Pink_ChargingCaseOpen.png',
-        image2: './images/JBL_TUNE220TWS_ProductImage_Pink_Back.png',
-        old_price: '400',
-        curr_price: '300'
-    },
-    {
-        name: 'UA Project Rock',
-        image1: './images/190402_E1_FW19_EarbudsWCase_S13_0033-1_1605x1605_HERO.png',
-        image2: './images/190402_E1_FW19_EarbudsWCase_S13_0033-1_1605x1605_BACK.png',
-        old_price: '400',
-        curr_price: '300'
-    },
-]
+  {
+    name: "JBL E55BT KEY BLACK",
+    image1: "../images/JBL_E55BT_KEY_BLACK_6175_FS_x1-1605x1605px.png",
+    image2: "../images/JBL_LIVE650BTNC_Product Image_Folded_Black.webp",
+    old_price: "400",
+    curr_price: "300",
+  },
+  {
+    name: "JBL JR 310BT",
+    image1: "../images/JBL_JR 310BT_Product Image_Hero_Skyblue.png",
+    image2: "../images/JBL_JR 310BT_Product Image_Detail_Skyblue.png",
+    old_price: "400",
+    curr_price: "300",
+  },
+  {
+    name: "JBL TUNE 750BTNC",
+    image1:
+      "../images/kisspng-beats-electronics-headphones-apple-beats-studio-red-headphones.png",
+    image2: "../images/JBL_E55BT_KEY_RED_6063_FS_x1-1605x1605px.webp",
+    old_price: "400",
+    curr_price: "300",
+  },
+  {
+    name: "JBL Horizon",
+    image1: "../images/JBLHorizon_001_dvHAMaster.png",
+    image2: "../images/JBLHorizon_BLK_002_dvHAMaster.webp",
+    old_price: "400",
+    curr_price: "300",
+  },
+  {
+    name: "JBL Tune 220TWS",
+    image1: "../images/JBL_TUNE220TWS_ProductImage_Pink_ChargingCaseOpen.png",
+    image2: "../images/JBL_TUNE220TWS_ProductImage_Pink_Back.png",
+    old_price: "400",
+    curr_price: "300",
+  },
+  {
+    name: "UA Project Rock",
+    image1:
+      "../images/190402_E1_FW19_EarbudsWCase_S13_0033-1_1605x1605_HERO.png",
+    image2:
+      "../images/190402_E1_FW19_EarbudsWCase_S13_0033-1_1605x1605_BACK.png",
+    old_price: "400",
+    curr_price: "300",
+  },
+];
 
-let product_list = document.querySelector('#products')
+let product_list = document.querySelector("#products");
 
 renderProducts = (products) => {
-    products.forEach(e => {
-        let prod = `
+  products.forEach((e) => {
+    let prod = `
             <div class="col-4 col-md-6 col-sm-12">
                 <div class="product-card">
                     <div class="product-card-img">
@@ -74,16 +77,20 @@ renderProducts = (products) => {
                     </div>
                 </div>
             </div>
-        `
-        product_list.insertAdjacentHTML("beforeend", prod)
-    })
-}
+        `;
+    product_list.insertAdjacentHTML("beforeend", prod);
+  });
+};
 
-renderProducts(products)
-renderProducts(products)
+renderProducts(products);
+renderProducts(products);
 
-let filter_col = document.querySelector('#filter-col')
+let filter_col = document.querySelector("#filter-col");
 
-document.querySelector('#filter-toggle').addEventListener('click', () => filter_col.classList.toggle('active'))
+document
+  .querySelector("#filter-toggle")
+  .addEventListener("click", () => filter_col.classList.toggle("active"));
 
-document.querySelector('#filter-close').addEventListener('click', () => filter_col.classList.toggle('active'))
+document
+  .querySelector("#filter-close")
+  .addEventListener("click", () => filter_col.classList.toggle("active"));
--- src/appRoutes.js
+++ src/appRoutes.js
@@ -1,24 +1,91 @@
 import express from "express";
 
-const routes = express.Router()
+const router = express.Router();
 
-
-routes.get('/', (req, res) => {
-    //?q=monggo
-    const { q } = req.query
-
-    const { title } = req.body
-
-    const data = {}
-    // bookService.create({ title: data.title, ...})
-    res.render('trang-chu')
-})
-routes.get('/books', (req, res) => {
-    res.render('index');
+router.get("/", function (req, res, next) {
+  res.render("trang-chu", { title: "Express", layout: "layouts/main" });
 });
-routes.get('/books/:id', (req, res) => {
-    const { id } = req.params
 
-    res.render('index');
+router.get("/chinh-sach-bao-mat", function (req, res, next) {
+  res.render("chinh-sach-bao-mat", {
+    title: "Express",
+    layout: "layouts/main",
+  });
 });
-export default routes
+
+router.get("/chinh-sach-doi-tra-hoan-tien", function (req, res, next) {
+  res.render("chinh-sach-doi-tra-hoan-tien", {
+    title: "Express",
+    layout: "layouts/main",
+  });
+});
+
+router.get("/chinh-sach-khach-si", function (req, res, next) {
+  res.render("chinh-sach-khach-si", {
+    title: "Express",
+    layout: "layouts/main",
+  });
+});
+
+router.get("/dieu-khoan-dich-vu", function (req, res, next) {
+  res.render("dieu-khoan-dich-vu", {
+    title: "Express",
+    layout: "layouts/main",
+  });
+});
+
+router.get("/gioi-thieu", function (req, res, next) {
+  res.render("gioi-thieu", {
+    title: "Express",
+    layout: "layouts/main",
+  });
+});
+
+router.get("/lien-he", function (req, res, next) {
+  res.render("lien-he", {
+    title: "Express",
+    layout: "layouts/main",
+  });
+});
+
+router.get("/phuong-thuc-van-chuyen", function (req, res, next) {
+  res.render("phuong-thuc-van-chuyen", {
+    title: "Express",
+    layout: "layouts/main",
+  });
+});
+
+router.get("/thoi-gian-lam-viec", function (req, res, next) {
+  res.render("thoi-gian-lam-viec", {
+    title: "Express",
+    layout: "layouts/main",
+  });
+});
+
+router.get("/category/tat-ca-san-pham", function (req, res, next) {
+  res.render("category/tat-ca-san-pham", {
+    layout: "layouts/main",
+  });
+});
+router.get("/category/may-nghe-nhac", function (req, res, next) {
+  res.render("category/may-nghe-nhac", {
+    layout: "layouts/main",
+  });
+});
+router.get("/category/loa", function (req, res, next) {
+  res.render("category/loa", {
+    layout: "layouts/main",
+  });
+});
+router.get("/category/microphone", function (req, res, next) {
+  res.render("category/microphone", {
+    layout: "layouts/main",
+  });
+});
+router.get("/category/tai-nghe", function (req, res, next) {
+  res.render("category/tai-nghe", {
+    layout: "layouts/main",
+  });
+});
+
+export default router;
