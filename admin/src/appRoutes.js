66d7b76c86195b7a0252d9fd98345c947d38f051
Nguyen Kha Vi <nguyenkhavi01032001@gmail.com>
2021-11-12 13:00:22 +0700

    login page

--- admin/src/appRoutes.js
+++ admin/src/appRoutes.js
@@ -5,38 +5,6 @@ import productRoutes from '../src/routes/productRoutes'
 import orderRoutes from '../src/routes/orderRoutes'
 const routes = express.Router()
 
-router.get("/", function (req, res, next) {
-    res.render("trang-chu", { title: "Express", layout: "layouts/main" });
-});
-
-router.get("/chinh-sach-bao-mat", function (req, res, next) {
-    res.render("chinh-sach-bao-mat", {
-        title: "Express",
-        layout: "layouts/main",
-    });
-});
-
-router.get("/chinh-sach-doi-tra-hoan-tien", function (req, res, next) {
-    res.render("chinh-sach-doi-tra-hoan-tien", {
-        title: "Express",
-        layout: "layouts/main",
-    });
-});
-
-router.get("/chinh-sach-khach-si", function (req, res, next) {
-    res.render("chinh-sach-khach-si", {
-        title: "Express",
-        layout: "layouts/main",
-    });
-});
-
-router.get("/dieu-khoan-dich-vu", function (req, res, next) {
-    res.render("dieu-khoan-dich-vu", {
-        title: "Express",
-        layout: "layouts/main",
-    });
-});
-
 routes.use('/', indexRoutes)
 routes.use('/users', userRoutes)
 routes.use('/products', productRoutes)
