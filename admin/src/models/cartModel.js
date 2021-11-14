8cdaab993ce3d12654a9128fff3c32cfef40c872
Nguyen Kha Vi <nguyenkhavi01032001@gmail.com>
2021-11-14 20:45:42 +0700

    Update db

--- admin/src/models/cartModel.js
+++ admin/src/models/cartModel.js
@@ -4,7 +4,7 @@ const { Schema } = mongoose;
 const schema = new Schema(
     {
         //sessionId: Đã đăng nhập ? id của user : id của session
-        sessionId: { type: Schema.Types.ObjectId | Schema.Types.String },
+        sessionId: { type: Schema.Types.ObjectId, required: true },
         //items: Mảng các item, mỗi item có { productId, price, quantity}
         items: {
             type: Array
--- admin/src/models/userModel.js
+++ admin/src/models/userModel.js
@@ -4,13 +4,13 @@ const { Schema } = mongoose;
 const schema = new Schema(
     {
         //name: Tên người dùng
-        name: { type: Schema.Types.String },
+        name: { type: Schema.Types.String, required: true },
         //username: Tên dùng để đăng nhập
-        username: { type: Schema.Types.String },
+        username: { type: Schema.Types.String, required: true },
         //password: mật khẩu đã được hash của người dùng
-        password: { type: Schema.Types.String },
+        password: { type: Schema.Types.String, required: true },
         //email: email của người dùng
-        email: { type: Schema.Types.String },
+        email: { type: Schema.Types.String, required: true },
         //address: địa chỉ của người dùng (chưa chắc là địa chỉ giao hàng)
         address: { type: Schema.Types.String },
         //birthday: sinh nhật người dùng
