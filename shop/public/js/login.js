5b5d7b5da694e117a902cc65275a69eb661369aa
Kouvuli <Kouvuli@users.noreply.github.com>
2021-11-16 17:13:27 +0700

    login commit

--- shop/public/js/login.js
+++ shop/public/js/login.js
@@ -4,10 +4,10 @@ var z = document.getElementById("btn");
 function register() {
   x.style.left = "-400px";
   y.style.left = "50px";
-  z.style.left = "135px";
+  z.style.left = "110px";
 }
 function login() {
   x.style.left = "50px";
   y.style.left = "450px";
-  z.style.left = "21px";
+  z.style.left = "5px";
 }
