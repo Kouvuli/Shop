e5c587be732c5b0d2d03c064629b13312f3a40b2
Kha-Vi Nguyen <nguyenkhavi01032001@gmail.com>
2022-01-16 16:44:21 +0700

    FIX

--- admin/src/services/orderService.js
+++ admin/src/services/orderService.js
@@ -3,6 +3,37 @@ import discountModel from "../models/discountModel";
 import userModel from "../models/userModel";
 
 const orderService = {
+    async getOrders({ page = 1, perPage = 10 }) {
+        const p = Math.max(parseInt(page), 1);
+        const pp = parseInt(perPage);
+        const origin = await orderModel
+            .find({})
+            .skip(pp * p - pp)
+            .limit(pp)
+            .lean();
+        const total = await orderModel.countDocuments();
+        let data = [];
+        for (const order of origin) {
+            const { name } = (await userModel.findById(order.userId)) || {};
+            let discountPercent = 0;
+            for (const discount of order.discounts) {
+                const { value } = await discountModel.findById(discount);
+                discountPercent += parseFloat(value);
+            }
+
+            let cost = 0;
+            for (const product of order.products) {
+                cost += parseFloat(product.price) * parseInt(product.quantity);
+            }
+            data.push({
+                ...order,
+                name,
+                discount: (discountPercent * cost) / 100,
+                cost,
+            });
+        }
+        return { data, page, perPage, total };
+    },
     async getSales() {
         const origin = await orderModel.find({}).lean();
         let total = 0;
