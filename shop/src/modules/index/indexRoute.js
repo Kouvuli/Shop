0460f12b05c36623284558b2425a3064b8512d83
Kouvuli <Kouvuli@users.noreply.github.com>
2022-01-17 10:46:54 +0700

    change view names and update UI

--- shop/src/modules/index/indexRoute.js
+++ shop/src/modules/index/indexRoute.js
@@ -3,60 +3,60 @@ import cartService from "../../services/cartService";
 const router = express.Router();
 
 router.get("/", async (req, res, next) => {
-    const userId = req.user;
-    if (!!req.session.cartId && !!userId) {
-        const cartId = req.session.cartId;
-        await cartService.setUserIdByCartId({ userId, cartId });
-        req.session.cartId = "";
-    }
-    res.render("home", { title: "Trang chủ" });
+  const userId = req.user;
+  if (!!req.session.cartId && !!userId) {
+    const cartId = req.session.cartId;
+    await cartService.setUserIdByCartId({ userId, cartId });
+    req.session.cartId = "";
+  }
+  res.render("home", { title: "Trang chủ" });
 });
 
 router.get("/intro", function (req, res, next) {
-    res.render("intro", {
-        title: "Giới thiệu",
-    });
+  res.render("intro", {
+    title: "Giới thiệu",
+  });
 });
 
 router.get("/contact", function (req, res, next) {
-    res.render("contact", {
-        title: "Liên hệ",
-    });
+  res.render("contact", {
+    title: "Liên hệ",
+  });
 });
-router.get("/chinh-sach-bao-mat", function (req, res, next) {
-    res.render("chinh-sach-bao-mat", {
-        title: "Express",
-    });
-});
-
-router.get("/chinh-sach-doi-tra-hoan-tien", function (req, res, next) {
-    res.render("chinh-sach-doi-tra-hoan-tien", {
-        title: "Express",
-    });
+router.get("/privacy-policy", function (req, res, next) {
+  res.render("shop/privacy-policy", {
+    title: "Chính sách bảo mật",
+  });
 });
 
-router.get("/chinh-sach-khach-si", function (req, res, next) {
-    res.render("chinh-sach-khach-si", {
-        title: "Express",
-    });
+router.get("/exchange-refund-policy", function (req, res, next) {
+  res.render("shop/exchange-refund-policy", {
+    title: "Chính sách đổi trả",
+  });
 });
 
-router.get("/dieu-khoan-dich-vu", function (req, res, next) {
-    res.render("dieu-khoan-dich-vu", {
-        title: "Express",
-    });
+router.get("/wholesale-customer-policy", function (req, res, next) {
+  res.render("shop/wholesale-customer-policy", {
+    title: "Chính sách khách sỉ",
+  });
 });
 
-router.get("/phuong-thuc-van-chuyen", function (req, res, next) {
-    res.render("phuong-thuc-van-chuyen", {
-        title: "Express",
-    });
+router.get("/terms-of-service", function (req, res, next) {
+  res.render("shop/terms-of-service", {
+    title: "Điều khoản dịch vụ",
+  });
 });
 
-router.get("/thoi-gian-lam-viec", function (req, res, next) {
-    res.render("thoi-gian-lam-viec", {
-        title: "Express",
-    });
+router.get("/shipping-method", function (req, res, next) {
+  res.render("shop/shipping-method", {
+    title: "Phương thức vận chuyển",
+  });
+});
+
+router.get("/working-time", function (req, res, next) {
+  res.render("shop/working-time", {
+    title: "Thời gian làm việc",
+  });
 });
 
 export default router;
