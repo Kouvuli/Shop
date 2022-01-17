cc7b49aa616cd74a153601e0018a3ab059ce96af
Kha-Vi Nguyen <nguyenkhavi01032001@gmail.com>
2022-01-17 13:19:35 +0700

    FIX: shop

--- shop/src/modules/index/indexRoute.js
+++ shop/src/modules/index/indexRoute.js
@@ -24,37 +24,37 @@ router.get("/contact", function (req, res, next) {
     });
 });
 router.get("/chinh-sach-bao-mat", function (req, res, next) {
-    res.render("chinh-sach-bao-mat", {
+    res.render("shop/chinh-sach-bao-mat", {
         title: "Express",
     });
 });
 
 router.get("/chinh-sach-doi-tra-hoan-tien", function (req, res, next) {
-    res.render("chinh-sach-doi-tra-hoan-tien", {
+    res.render("shop/chinh-sach-doi-tra-hoan-tien", {
         title: "Express",
     });
 });
 
 router.get("/chinh-sach-khach-si", function (req, res, next) {
-    res.render("chinh-sach-khach-si", {
+    res.render("shop/chinh-sach-khach-si", {
         title: "Express",
     });
 });
 
 router.get("/dieu-khoan-dich-vu", function (req, res, next) {
-    res.render("dieu-khoan-dich-vu", {
+    res.render("shop/dieu-khoan-dich-vu", {
         title: "Express",
     });
 });
 
 router.get("/phuong-thuc-van-chuyen", function (req, res, next) {
-    res.render("phuong-thuc-van-chuyen", {
+    res.render("shop/phuong-thuc-van-chuyen", {
         title: "Express",
     });
 });
 
 router.get("/thoi-gian-lam-viec", function (req, res, next) {
-    res.render("thoi-gian-lam-viec", {
+    res.render("shop/thoi-gian-lam-viec", {
         title: "Express",
     });
 });
