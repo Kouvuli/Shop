b49d16d4fda59aee6ab406dffa3ac9288ef398ce
Kha-Vi Nguyen <nguyenkhavi01032001@gmail.com>
2022-01-22 15:14:05 +0700

    Update: Update product

--- admin/src/modules/product/productControllers.js
+++ admin/src/modules/product/productControllers.js
@@ -1,85 +1,159 @@
-
-import _ from 'lodash'
-import helpers from '../../helpers'
-import firebaseService from '../../services/firebaseService'
-import productService from '../../services/productService'
+import _ from "lodash";
+import helpers from "../../helpers";
+import firebaseService from "../../services/firebaseService";
+import productService from "../../services/productService";
 const productControllers = {
     async index(req, res) {
-
-        const { page = 1, perPage = 10, type = "", q = "" } = req.query
-        const { data, total } = await productService.getProducts({ q, page, perPage })
+        const { page = 1, perPage = 10, type = "", q = "" } = req.query;
+        const { data, total } = await productService.getProducts({
+            q,
+            page,
+            perPage,
+        });
 
         const state = {
-            title: 'Sản phẩm',
+            title: "Sản phẩm",
             page,
             perPage: Math.min(perPage, data.length),
             data,
             total,
-            header: ["Tên sản phẩm", "Danh mục", "Hãng sản xuất", "Giá nhập", "Giá bán", "Số lượng", "Mô tả", "Hình ảnh"]
-        }
+            header: [
+                "Tên sản phẩm",
+                "Danh mục",
+                "Hãng sản xuất",
+                "Giá nhập",
+                "Giá bán",
+                "Số lượng",
+                "Mô tả",
+                "Hình ảnh",
+            ],
+        };
 
-        res.render('products/index', { ...state, pagination: { page, limit: Math.ceil(total / perPage), perPage: perPage } })
+        res.render("products/index", {
+            ...state,
+            pagination: {
+                page,
+                limit: Math.ceil(total / perPage),
+                perPage: perPage,
+            },
+        });
     },
 
     async topSeller(req, res) {
-
-        const { page = 1, perPage = 10, type = "" } = req.query
-        const { data, total } = await productService.getTopSellers({ page, perPage, type })
+        const { page = 1, perPage = 10, type = "" } = req.query;
+        const { data, total } = await productService.getTopSellers({
+            page,
+            perPage,
+            type,
+        });
 
         const state = {
-            title: 'Top 10 sản phẩm bán chạy nhất',
+            title: "Top 10 sản phẩm bán chạy nhất",
             page,
             total,
             perPage,
             data,
-            header: ["Tên sản phẩm", "Danh mục", "Hãng sản xuất", "Giá nhập", "Giá bán", "Đã bán", "Mô tả", "Hình ảnh"]
-        }
+            header: [
+                "Tên sản phẩm",
+                "Danh mục",
+                "Hãng sản xuất",
+                "Giá nhập",
+                "Giá bán",
+                "Đã bán",
+                "Mô tả",
+                "Hình ảnh",
+            ],
+        };
 
-        res.render('products/top', { ...state, pagination: { page, limit: Math.ceil(total / perPage), perPage: perPage } })
+        res.render("products/top", {
+            ...state,
+            pagination: {
+                page,
+                limit: Math.ceil(total / perPage),
+                perPage: perPage,
+            },
+        });
     },
 
     async createProduct(req, res) {
         if (!_.isEmpty(req.body)) {
-            const { name, categoryType: type, description, manufacturer: manufacturerName, originPrice, currentPrice, quantity } = req.body
+            const {
+                name,
+                categoryType: type,
+                description,
+                manufacturer: manufacturerName,
+                originPrice,
+                currentPrice,
+                quantity,
+            } = req.body;
             const images = await firebaseService.uploadMultipleFiles(req.files);
-            await productService.createProduct({ name, description, category: { type }, manufacturer: { name: manufacturerName }, quantity, originPrice, currentPrice, images })
-            return res.redirect('/products/create')
-
+            await productService.createProduct({
+                name,
+                description,
+                category: { type },
+                manufacturer: { name: manufacturerName },
+                quantity,
+                originPrice,
+                currentPrice,
+                images,
+            });
+            return res.redirect("/products/create");
         }
-        const title = "Thêm sản phẩm"
-        res.render('products/create', { title })
+        const title = "Thêm sản phẩm";
+        res.render("products/create", { title });
     },
     async editProduct(req, res) {
-        const { id } = req.params
-        const title = "Cập nhật sản phẩm"
+        const { id } = req.params;
+        const old = await productService.getProductById({ id });
+        const title = "Cập nhật sản phẩm";
         if (_.isEmpty(req.body)) {
-            const product = await productService.getProductById({ id })
-            res.render('products/edit', { product, image1: product.images[0] || "", image2: product?.images[1] || "", title })
+            const product = await productService.getProductById({ id });
+            res.render("products/edit", {
+                product,
+                image1: product.images[0] || "",
+                image2: product?.images[1] || "",
+                title,
+            });
         } else {
-            const { name, categoryType: type, description, manufacturer: manufacturerName, originPrice, currentPrice, quantity } = req.body
+            const {
+                name,
+                categoryType: type,
+                description,
+                manufacturer: manufacturerName,
+                originPrice,
+                currentPrice,
+                quantity,
+            } = req.body;
             const images = await firebaseService.uploadMultipleFiles(req.files);
-            await productService.updateProductById({ id, name, description, category: { type }, manufacturer: { name: manufacturerName }, quantity, originPrice, currentPrice, images })
-            return res.redirect('/products')
+            await productService.updateProductById({
+                id,
+                name,
+                description,
+                category: { type },
+                manufacturer: { name: manufacturerName },
+                quantity,
+                originPrice,
+                currentPrice,
+                images: images.length > 0 ? images : old.images,
+            });
+            return res.redirect("/products");
         }
-
     },
     async confirmScreen(req, res) {
-        const { id = "" } = req.params
+        const { id = "" } = req.params;
 
-        const product = await productService.getProductById({ id })
+        const product = await productService.getProductById({ id });
 
-        res.render('products/confirm', { productName: product.name, id })
-    }
-    ,
+        res.render("products/confirm", { productName: product.name, id });
+    },
     async deleteProduct(req, res) {
-        const { id = "", status = "canceled" } = req.params
+        const { id = "", status = "canceled" } = req.params;
 
         if (status === "accepted") {
-            await productService.deleteProductById({ id })
+            await productService.deleteProductById({ id });
         }
-        return res.redirect('/products')
+        return res.redirect("/products");
+    },
+};
 
-    }
-}
-
-export default productControllers
+export default productControllers;
